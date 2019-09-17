f45b6ec5b8e5a176b118894d07f9ef7227ba768c
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-17 21:12:58 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+  # Setter for breed
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end 
+  
+  # Getter for breed
+  def breed 
+    @breed
+  end 
+  
+end 
+
+
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  
+  # Setter for job 
+  def job=(person_job)
+    @job = person_job
+  end 
+  
+  # Getter for job 
+  def job 
+    @job
+  end 
+  
+end 
+
